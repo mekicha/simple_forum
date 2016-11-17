610ba3415ae00c890e1868bd29f074f6f891dff0
mekicha <emeka.icha@gmail.com>
2016-11-17 12:01:28 +0300

    Create api endpoints to GET, POST, PUT and DELETE messages

---
+++ routes/messages.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/simple_forum');
+
+/*** Endpoint that returns the list of all messages
+     in the forum as a list of <ID,HEADER> pair.
+     Note: We are using middleware to make the endpoint
+     available at /api/messages.--> See app.js
+***/
+router.get('/', function(req, res) {
+	var collection = db.get('messages'); //table is messages
+	collection.find({}, function(err, messages) {
+		if (err) throw err;
+		var msgs = [];
+		for(var i = 0; i < messages.length; i++){
+		    msgs.push([messages[i]._id ,messages[i].header]);
+		}
+		res.json(msgs);
+
+  });
+});
+
+/*** Create a new post.
+     @@params: header : the title of the message
+              message: the message body.
+              id: Message id...automatically assigned
+              by the mongo engine.
+***/
+router.post('/', function(req, res) {
+	var collection = db.get('messages');
+	collection.insert({
+		header: req.body.header,
+		message: req.body.message
+
+	}, function(err, message) {
+		if (err) throw err;
+		res.json(message);
+
+	});
+});
+
+/*** Return message body given an id of the message.
+     @@params id: id of the message to be returned.
+     message: the body of the message that is returned.
+***/
+router.get('/:id', function(req, res) {
+	var collection = db.get('messages');
+	collection.findOne({_id: req.params.id}, function(err, message) {
+		if (err) throw err;
+
+		res.json(message.message);
+	});
+});
+
+
+/***PUT /api/videos/{id}
+    Edit message given an id.
+    @@params id: id of message to be edited.
+    return value: Success message, else error if
+    edit could not be performed.
+***/
+router.put('/:id', function(req, res) {
+	var collection = db.get('messages');
+	collection.update({
+		_id: req.params.id
+	},
+	{
+		header: req.body.header,
+		message: req.body.message
+
+	}, function(err, message) {
+		if(err) throw err;
+
+		res.json(message);
+	});
+});
+
+/****Delete a message, given an id
+     @@params: id- the id of the message to be deleted.
+     return: success if deleted or error if message
+     could not be deleted.
+****/
+
+router.delete('/:id', function(req, res) {
+	var collection = db.get('videos');
+	collection.remove({_id: req.params.id}, function(err, video) {
+		if (err) throw err;
+
+		res.json(video);
+	});
+});
+
+module.exports = router;
